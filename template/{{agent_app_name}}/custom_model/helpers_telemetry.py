2acf115cbcd4b31fca7c62e16bdd4b1641ff565c
mjnitz02 <matt.nitzken@datarobot.com>
2025-05-19 09:32:11 -0400

    Lint

--- template/{{agent_app_name}}/custom_model/helpers_telemetry.py
+++ template/{{agent_app_name}}/custom_model/helpers_telemetry.py
@@ -1,7 +1,7 @@
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.instrumentation.aiohttp_client import AioHttpClientInstrumentor
 from opentelemetry.instrumentation.crewai import CrewAIInstrumentor
 from opentelemetry.instrumentation.openai import OpenAIInstrumentor
-from opentelemetry.instrumentation.aiohttp_client import AioHttpClientInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
 instrument_requests = RequestsInstrumentor().instrument()
 instrument_aiohttp = AioHttpClientInstrumentor().instrument()
