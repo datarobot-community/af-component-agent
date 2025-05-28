044d3b471292087dfa64c7b750da271b1e8fc4eb
mjnitz02 <matt.nitzken@datarobot.com>
2025-05-28 10:23:32 -0400

    Revert custom.py

--- template/{{agent_app_name}}/custom_model/custom.py
+++ template/{{agent_app_name}}/custom_model/custom.py
@@ -18,13 +18,11 @@ from helpers_telemetry import *  # noqa # pylint: disable=unused-import
 from agent import MyAgent
 from auth import initialize_authorization_context
 from helpers import (
-    CrewAIEventListener,
     CustomModelChatResponse,
     create_inputs_from_completion_params,
     to_custom_model_response,
 )
 from openai.types.chat import CompletionCreateParams
-from ragas.messages import AIMessage
 
 
 def load_model(code_dir: str) -> str:
@@ -64,9 +62,6 @@ def chat(
     # access tokens for external services.
     initialize_authorization_context(completion_create_params)
 
-    # Initalize CrewAI Event listener
-    event_listener = CrewAIEventListener()
-
     # Instantiate the agent, all fields from the completion_create_params are passed to the agent
     # allowing environment variables to be passed during execution
     agent = MyAgent(**completion_create_params)
@@ -75,11 +70,8 @@ def chat(
     inputs = create_inputs_from_completion_params(completion_create_params)
 
     # Execute the agent with the inputs
-    crew_output = agent.run(inputs=inputs)
-    response_text = str(crew_output.raw)
-    events = event_listener.messages
-    last_message = events[-1].content
-    if last_message != response_text:
-        events.append(AIMessage(content=response_text))
+    agent_result = agent.run(inputs=inputs)
 
-    return to_custom_model_response(events, crew_output)
+    if isinstance(agent_result, tuple):
+        return to_custom_model_response(*agent_result)
+    return to_custom_model_response(agent_result)
