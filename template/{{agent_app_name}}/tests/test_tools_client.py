2e76e530f9cd7e2ca52416e5e32f87642e243bc7
Marcin Dambski <marcin.dambski@datarobot.com>
2025-06-06 16:44:08 +0200

    add configuration tests

--- template/{{agent_app_name}}/tests/test_tools_client.py
+++ template/{{agent_app_name}}/tests/test_tools_client.py
@@ -17,20 +17,21 @@ from unittest.mock import patch
 import pytest
 from tools_client import ToolClient
 
+application_base_url = "https://example.com"
 
 @pytest.mark.parametrize("base_url", [
-    "https://example.com/api/v2",
-    "https://example.com/api/v2/",
-    "https://example.com",
-    "https://example.com/"
+    f"{application_base_url}/api/v2",
+    f"{application_base_url}/api/v2/",
+    f"{application_base_url}",
+    f"{application_base_url}/"
 ])
 @pytest.mark.parametrize("api_key", ["test-api-key"])
 def test_tool_client_config(base_url, api_key):
 
     def _assert(tool_client):
         assert tool_client.api_key == api_key
-        assert tool_client.base_url == "https://example.com"
-        assert tool_client.datarobot_api_endpoint == "https://example.com/api/v2"
+        assert tool_client.base_url == application_base_url
+        assert tool_client.datarobot_api_endpoint == f"{application_base_url}/api/v2"
 
     _assert(ToolClient(api_key=api_key, base_url=base_url))
 
@@ -40,3 +41,10 @@ def test_tool_client_config(base_url, api_key):
     }
     with patch.dict(os.environ, overrides, clear=True):
         _assert(ToolClient())
+
+
+def test_tool_client_config_defaults():
+    tool_client = ToolClient()
+    assert tool_client.api_key is None
+    assert tool_client.base_url == "https://app.datarobot.com"
+    assert tool_client.datarobot_api_endpoint == "https://app.datarobot.com/api/v2"
