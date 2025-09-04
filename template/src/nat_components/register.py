2d9c5df0cb2d4e9f6d52050df836a88abd9defd3
James Clemens <james.clemens@datarobot.com>
2025-09-04 12:02:12 -0400

    placeholder functions

--- template/src/nat_components/register.py
+++ template/src/nat_components/register.py
@@ -1,150 +1,53 @@
 import logging 
 
-from pydantic import Field, BaseModel
-from nat_components.tools import add, multiply, subtract, divide, power, modulo, sin, cos, tan
-
+from pydantic import Field
+from nat_components.tools import plan, write, edit
 from nat.builder.builder import Builder
-from nat.builder.function import LambdaFunction
-from nat.builder.framework_enum import LLMFrameworkEnum
 from nat.builder.function_info import FunctionInfo
 from nat.cli.register_workflow import register_function
-from nat.data_models.component_ref import FunctionRef
-from nat.data_models.component_ref import LLMRef
 from nat.data_models.function import FunctionBaseConfig
 
 logger = logging.getLogger(__name__)
 
 
+class PlanConfig(FunctionBaseConfig, name="plan"):
+    description: str = Field(default=plan.__doc__)
 
 
-# Create a function info
-add_function_info = FunctionInfo.from_fn(
-    add,
-    description=add.__doc__
-)
-
-class AddConfig(FunctionBaseConfig, name="add"):
-    description: str = Field(default=add.__doc__)
-
-
-@register_function(config_type=AddConfig)
-async def adder(config: AddConfig, builder: Builder):
+@register_function(config_type=PlanConfig)
+async def planner(config: PlanConfig, builder: Builder):
 
 
     # Yield the function info object which will be used to create a function
     yield FunctionInfo.from_fn(
-        add,
-        description=add.__doc__
+        plan,
+        description=plan.__doc__
     )
 
 
-class MultiplyConfig(FunctionBaseConfig, name="multiply"):
-    description: str = Field(default=multiply.__doc__)
+class WriteConfig(FunctionBaseConfig, name="write"):
+    description: str = Field(default=write.__doc__)
 
 
-@register_function(config_type=MultiplyConfig)
-async def multiplier(config: MultiplyConfig, builder: Builder):
+@register_function(config_type=WriteConfig)
+async def writer(config: WriteConfig, builder: Builder):
 
     # Yield the function info object which will be used to create a function
     yield FunctionInfo.from_fn(
-        multiply,
-        description=multiply.__doc__
+        write,
+        description=write.__doc__
     )
 
 
-class SubtractConfig(FunctionBaseConfig, name="subtract"):
-    description: str = Field(default=subtract.__doc__)
+class EditConfig(FunctionBaseConfig, name="edit"):
+    description: str = Field(default=edit.__doc__)
 
 
-@register_function(config_type=SubtractConfig)
-async def subtractor(config: SubtractConfig, builder: Builder):
-
-    # Yield the function info object which will be used to create a function
-    yield FunctionInfo.from_fn(
-        subtract,
-        description=subtract.__doc__
-    )
-
-
-class DivideConfig(FunctionBaseConfig, name="divide"):
-    description: str = Field(default=divide.__doc__)
-
-
-@register_function(config_type=DivideConfig)
-async def divider(config: DivideConfig, builder: Builder):
+@register_function(config_type=EditConfig)
+async def editor(config: EditConfig, builder: Builder):
 
     # Yield the function info object which will be used to create a function
     yield FunctionInfo.from_fn(
-        divide,
-        description=divide.__doc__
-    )
-
-
-class PowerConfig(FunctionBaseConfig, name="power"):
-    description: str = Field(default=power.__doc__)
-
-
-@register_function(config_type=PowerConfig)
-async def power_calculator(config: PowerConfig, builder: Builder):
-
-    # Yield the function info object which will be used to create a function
-    yield FunctionInfo.from_fn(
-        power,
-        description=power.__doc__
-    )
-
-
-class ModuloConfig(FunctionBaseConfig, name="modulo"):
-    description: str = Field(default=modulo.__doc__)
-
-
-@register_function(config_type=ModuloConfig)
-async def modulo_calculator(config: ModuloConfig, builder: Builder):
-
-    # Yield the function info object which will be used to create a function
-    yield FunctionInfo.from_fn(
-        modulo,
-        description=modulo.__doc__
-    )
-
-
-class SinConfig(FunctionBaseConfig, name="sin"):
-    description: str = Field(default=sin.__doc__)
-
-
-@register_function(config_type=SinConfig)
-async def sin_calculator(config: SinConfig, builder: Builder):
-
-    # Yield the function info object which will be used to create a function
-    yield FunctionInfo.from_fn(
-        sin,
-        description=sin.__doc__
-    )
-
-
-class CosConfig(FunctionBaseConfig, name="cos"):
-    description: str = Field(default=cos.__doc__)
-
-
-@register_function(config_type=CosConfig)
-async def cos_calculator(config: CosConfig, builder: Builder):
-
-    # Yield the function info object which will be used to create a function
-    yield FunctionInfo.from_fn(
-        cos,
-        description=cos.__doc__
-    )
-
-
-class TanConfig(FunctionBaseConfig, name="tan"):
-    description: str = Field(default=tan.__doc__)
-
-
-@register_function(config_type=TanConfig)
-async def tan_calculator(config: TanConfig, builder: Builder):
-
-    # Yield the function info object which will be used to create a function
-    yield FunctionInfo.from_fn(
-        tan,
-        description=tan.__doc__
+        edit,
+        description=edit.__doc__
     )
--- template/src/nat_components/tools.py
+++ template/src/nat_components/tools.py
@@ -1,54 +1,12 @@
-from pydantic import BaseModel
-import math
 
-Numeric = float | int
+async def plan(topic: str) -> str:
+    """Plans a blog post for a topic"""
+    return topic
 
-class NatCalculatorInputs(BaseModel):
-    x: Numeric
-    y: Numeric
+async def write(outline: str) -> str:
+    """Writes a blog post based on the outline"""
+    return outline
 
-async def multiply(inputs: NatCalculatorInputs) -> Numeric:
-    """Multiplys the input x * Y requires both input to be 
-    float or int"""
-    return inputs.x * inputs.y
-
-async def add(inputs: NatCalculatorInputs) -> Numeric:
-    """Adds the input x + Y requires both input to be 
-    float or int"""
-    return inputs.x + inputs.y
-
-async def subtract(inputs: NatCalculatorInputs) -> Numeric:
-    """subtracks the input x  Y requires both input to be 
-    float or int"""
-    return inputs.x - inputs.y
-
-async def divide(inputs: NatCalculatorInputs) -> Numeric:
-    """divides the input x/Y requires both input to be 
-    float or int"""
-    return inputs.x / inputs.y
-
-async def power(inputs: NatCalculatorInputs) -> Numeric:
-    """Calculates x raised to the power of y (x^y) requires both input to be 
-    float or int"""
-    return inputs.x ** inputs.y
-
-async def modulo(inputs: NatCalculatorInputs) -> Numeric:
-    """Calculates the remainder of x divided by y (x % y) requires both input to be 
-    float or int"""
-    return inputs.x % inputs.y
-
-async def sin(inputs: NatCalculatorInputs) -> Numeric:
-    """Calculates the sine of x (in radians)
-    Uses only the x input, y is ignored"""
-    return math.sin(inputs.x)
-
-async def cos(inputs: NatCalculatorInputs) -> Numeric:
-    """Calculates the cosine of x (in radians)
-    Uses only the x input, y is ignored"""
-    return math.cos(inputs.x)
-
-async def tan(inputs: NatCalculatorInputs) -> Numeric:
-    """Calculates the tangent of x (in radians)
-    Uses only the x input, y is ignored"""
-    return math.tan(inputs.x)
-
+async def edit(blog_post: str) -> str:
+    """Edits the blog post"""
+    return blog_post
