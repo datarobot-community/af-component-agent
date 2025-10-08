7cd2dee24ad153319ff40e27a9bf335c512f3863
Carson Gee <carson.gee@datarobot.com>
2025-10-08 17:31:42 -0600

    Added logic to satisfy test

--- fixtures/infra/llm.py
+++ fixtures/infra/llm.py
@@ -1,5 +1,17 @@
+import os
+
 import pulumi_datarobot
 
 custom_model_runtime_parameters: list[
     pulumi_datarobot.CustomModelRuntimeParameterValueArgs
 ] = []
+
+# Add LLM deployment ID as a runtime parameter if set
+if os.environ.get("LLM_DATAROBOT_DEPLOYMENT_ID"):
+    custom_model_runtime_parameters.append(
+        pulumi_datarobot.CustomModelRuntimeParameterValueArgs(
+            key="LLM_DATAROBOT_DEPLOYMENT_ID",
+            type="string",
+            value=os.environ["LLM_DATAROBOT_DEPLOYMENT_ID"],
+        )
+    )
