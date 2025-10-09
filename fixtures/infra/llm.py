4b43396cf7ff950f43ade0abad52a19bd0034b99
Carson Gee <carson.gee@datarobot.com>
2025-10-09 12:39:44 -0600

    Add LLM as dependency for af-component-agent

--- fixtures/infra/llm.py
+++ fixtures/infra/llm.py
@@ -7,11 +7,11 @@ custom_model_runtime_parameters: list[
 ] = []
 
 # Add LLM deployment ID as a runtime parameter if set
-if os.environ.get("LLM_DATAROBOT_DEPLOYMENT_ID"):
+if os.environ.get("LLM_DEPLOYMENT_ID"):
     custom_model_runtime_parameters.append(
         pulumi_datarobot.CustomModelRuntimeParameterValueArgs(
-            key="LLM_DATAROBOT_DEPLOYMENT_ID",
+            key="LLM_DEPLOYMENT_ID",
             type="string",
-            value=os.environ["LLM_DATAROBOT_DEPLOYMENT_ID"],
+            value=os.environ["LLM_DEPLOYMENT_ID"],
         )
     )
