67873dd9ee923fc0e6290f5d66bef35676d30712
Andrii Shchur <andrii.shchur@datarobot.com>
2025-12-05 12:59:34 +0200

    try to fix mypy error

--- template/{{agent_app_name}}/lit.py
+++ template/{{agent_app_name}}/lit.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# mypy: disable-error-code="ignore-without-code"
+
 import chainlit as cl
 from openai import AsyncOpenAI
 
