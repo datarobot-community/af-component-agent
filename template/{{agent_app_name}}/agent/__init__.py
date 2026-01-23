26823d54221c6893d2a275266701762178739a52
cavitcakir <cavit.cakir@datarobot.com>
2026-01-23 16:54:05 +0100

    remove agentic_workflow

--- template/{{agent_app_name}}/agent/__init__.py
+++ template/{{agent_app_name}}/agent/__init__.py
@@ -11,3 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from agent.config import Config
+from agent.myagent import MyAgent
+
+__all__ = ["MyAgent", "Config"]
+
