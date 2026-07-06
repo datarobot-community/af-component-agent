2a0a588ecc6801dc030b000a60b34ccda288c976
Anatolii Stehnii <anatolii.stehnii@datarobot.com>
2026-07-06 15:17:39 +0300

    Fix tests

--- tests/test-cli.sh
+++ tests/test-cli.sh
@@ -75,7 +75,6 @@ fi
 echo "Initial execution"
 uvx --from go-task-bin task agent:cli -- \
     execute \
-    --show_output \
     --user_prompt '{"topic": "Artificial Intelligence"}' \
     > ./agent/output.log 2>&1
 cat ./agent/output.log
@@ -105,64 +104,18 @@ if cat ./agent/output.log | grep -q 'Running CLI execute' ; then
 fi
 
 # Check the execution result
-if cat ./agent/output.log | grep -q 'Execution result:' ; then
+if cat ./agent/output.log | grep -q 'Workflow Result:' ; then
     echo "Test passed: cli.py returned log containing execution result"
     else
     echo "Test failed: cli.py did not return log containing execution result"
     exit 1
 fi
 
-# Check the chat completion returned
-if cat ./agent/output.log | grep -q '"model": "unknown"' ; then
-    echo "Test passed: cli.py returned log containing chat completion"
-    else
-    echo "Test failed: cli.py did not return log containing chat completion"
-    exit 1
-fi
-
-# Check the chat completion returned
-if cat ./agent/output.log | grep -q '"content": "streaming success"' ; then
-    echo "Test passed: cli.py returned chat completion success"
-    else
-    echo "Test failed: cli.py did not return chat completion success"
-    exit 1
-fi
-
 # Run the CLI command with a sample user prompt
 echo "Initial execution"
 uvx --from go-task-bin task agent:cli -- \
     execute \
-    --show_output \
-    --completion_json "example-completion.json" \
-    > ./agent/output.log 2>&1
-cat ./agent/output.log
-
-# Check if the log file was created
-if [ $(wc -l < ./agent/output.log) -ge 13 ] ; then
-    echo "Log file created successfully and file not empty."
-    echo ""
-    echo "Contents of output.log:"
-    cat ./agent/output.log
-    else
-    echo "Log file was not created."
-    exit 1
-fi
-
-# Check the chat completion returned
-if cat ./agent/output.log | grep -q '"content": "streaming success"' ; then
-    echo "Test passed: cli.py returned chat completion success"
-    else
-    echo "Test failed: cli.py did not return chat completion success"
-    exit 1
-fi
-
-# Run the CLI command with a sample user prompt
-echo "Initial execution"
-uvx --from go-task-bin task agent:cli -- \
-    execute \
-    --show_output \
     --user_prompt '{"topic": "Artificial Intelligence"}' \
-    --stream \
     > ./agent/output.log 2>&1
 cat ./agent/output.log
 
@@ -178,7 +131,7 @@ if [ $(wc -l < ./agent/output.log) -ge 13 ] ; then
 fi
 
 # Check the streaming returned
-if cat ./agent/output.log | grep -q '"content": "streaming success"' ; then
+if cat ./agent/output.log | grep -q 'Workflow Result:' ; then
     echo "Test passed: cli.py returned streaming success"
     else
     echo "Test failed: cli.py did not return streaming success"
