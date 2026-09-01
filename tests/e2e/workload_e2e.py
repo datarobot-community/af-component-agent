5228fcd5857c148b947b8a72f9682e39fff751ee
oksanatkach <oksana.tkach@datarobot.com>
2026-09-01 22:24:08 +0300

    adding back higher level pulumi logs

--- tests/e2e/workload_e2e.py
+++ tests/e2e/workload_e2e.py
@@ -655,7 +655,7 @@ def run_workload_agent_e2e(
         # trigger -> build poll -> workload create.
         run_cmd(
             # task_cmd("deploy", "--", "--yes", "--skip-preview"),
-            task_cmd("deploy", "--", "--yes", "--skip-preview", "-v=9", "--logtostderr"),
+            task_cmd("deploy", "--", "--yes", "--skip-preview", "-v=3", "--logtostderr"),
             cwd=rendered_dir,
             timeout_seconds=PULUMI_UP_TIMEOUT_S,
         )
